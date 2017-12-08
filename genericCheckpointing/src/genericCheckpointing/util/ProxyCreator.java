aa95695921841127964a7f19079e97d48d54c741
poorvitusam <poorvitusam@gmail.com>
2017-12-08 03:08:40 -0500

    proxyCreator added

--- genericCheckpointing/src/genericCheckpointing/util/ProxyCreator.java
+++ genericCheckpointing/src/genericCheckpointing/util/ProxyCreator.java
@@ -1,5 +1,22 @@
 package genericCheckpointing.util;
 
-public class ProxyCreator {
+import genericCheckpointing.server.StoreRestoreI;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class ProxyCreator
+{
+
+	public StoreRestoreI createProxy(Class<?>[] interfaceArray, InvocationHandler handler){
+		StoreRestoreI storeRestoreRef =
+            (StoreRestoreI)
+            Proxy.newProxyInstance(
+                                   getClass().getClassLoader(),
+                                   interfaceArray,
+                                   handler
+                                   );
+
+		return storeRestoreRef;
+	}
 }
