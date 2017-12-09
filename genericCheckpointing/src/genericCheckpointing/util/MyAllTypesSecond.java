05a30c3e06f9e2a27d017e7dccad7e63fe94d719
poorvitusam <poorvitusam@gmail.com>
2017-12-09 01:24:04 -0500

    Base templates update

--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
@@ -1,5 +1,15 @@
 package genericCheckpointing.util;
 
-public class MyAllTypesSecond {
+public class MyAllTypesSecond extends SerializableObject{
+	private int myInt;
+
+	public int getMyInt() {
+		return myInt;
+	}
+
+	public void setMyInt(int myInt) {
+		this.myInt = myInt;
+	}
+	
 
 }
--- genericCheckpointing/src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
+++ genericCheckpointing/src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
@@ -1,5 +1,15 @@
 package genericCheckpointing.xmlStoreRestore;
 
-public class StoreRestoreHandler {
+import java.lang.reflect.*;
 
+public class StoreRestoreHandler implements InvocationHandler {
+
+	public Object invoke(Object proxy, Method method, Object[] args) {
+		try {
+			
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
